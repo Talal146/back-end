02d5d50e23877814de0d318f69934c1847c6d378
yusefzaman <yusef96@gmail.com>
2024-06-12 11:07:32 +0300

    creation of review  model

---
+++ models/Review.js
@@ -0,0 +1,22 @@
+const { Schema } = require('mongoose')
+
+const reviewSchema = new Schema(
+  {
+    reviewContent: { type: String, required: true },
+    reviewRating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 5
+    },
+    trainee: {
+      type: Schema.Types.ObjectId,
+      ref: 'Trainee'
+    },
+    name: String,
+  },
+  {
+    timestamps: true
+  }
+)
+module.exports = reviewSchema
