15ac4949a1c84c0a76b381acc77796d2719084fd
Abdullah <aboood23.ae@gmail.com>
2024-06-13 10:45:27 +0300

    Wrokout Router and Model updated

--- models/Workout.js
+++ models/Workout.js
@@ -1,11 +1,33 @@
-const { Schema } = require('mongoose')
+const Schema = mongoose.Schema
 
-const workoutSchema = new Schema({
-  name: { type: String, required: true },
-  machine: { type: String, required: true },
-  weight: { type: Number, required: true },
-  reps: { type: Number, required: true },
-  sets: { type: Number, required: true }
-})
+const workoutSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    machine: {
+      type: String,
+      required: true
+    },
+    reps: {
+      type: Number,
+      required: true
+    },
+    sets: {
+      type: Number,
+      required: true
+    },
+    weight: {
+      type: Number,
+      required: true
+    },
+    user_id: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+)
 
-module.exports = workoutSchema
+module.exports = mongoose.model('Workout', workoutSchema)
