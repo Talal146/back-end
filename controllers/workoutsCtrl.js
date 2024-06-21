7afbfcaa977c4164a1a9c04297ec5b722f9329ab
yusefzaman <yusef96@gmail.com>
2024-06-21 12:51:04 +0100

    update of workouts

--- controllers/workoutsCtrl.js
+++ controllers/workoutsCtrl.js
@@ -1,133 +1,144 @@
-const { Workout } = require('../models');
-const { Review } = require('../models');
+const { Workout } = require('../models')
+const { Review } = require('../models')
 
 async function getAllWorkouts(req, res) {
-	try {
-		let workouts = await Workout.find({});
-		res.send(workouts);
-	} catch (error) {
-		console.error('Error fetching workouts:', error);
-	}
+  try {
+    let workouts = await Workout.find({})
+    res.send(workouts)
+  } catch (error) {
+    console.error('Error fetching workouts:', error)
+  }
 }
 
-async function create(req, res) {
-	try {
-		const workout = new Workout(req.body);
-		if (workout.name) {
-			workout.name = workout.name.toUpperCase();
-		}
-		if (req.body.machine) {
-			workout.machine = workout.machine.toUpperCase();
-		}
-		if (req.body.weight) {
-			workout.weight = parseInt(workout.weight);
-		}
-		if (req.body.reps) {
-			workout.reps = parseInt(workout.reps);
-		}
-		if (req.body.sets) {
-			workout.sets = parseInt(workout.sets);
-		}
+// async function getOneWorkouts(req, res) {
+// 	// try {
+// 	// 	let workouts = await Workout.findById({});
+// 	// 	res.send(workouts);
+// 	// } catch (error) {
+// 	// 	console.error('Error fetching workouts:', error);
+// 	// }
+// }
 
-		const newWorkout = await workout.save();
-		console.log(`newWorkout ==> ${newWorkout}`);
-		res.send(newWorkout);
-	} catch (error) {
-		console.error('Error creating workout', error);
-	}
+async function createOneWorkout(req, res) {
+  try {
+    const workout = new Workout(req.body)
+    if (workout.name) {
+      workout.name = workout.name.toUpperCase()
+    }
+    if (req.body.machine) {
+      workout.machine = workout.machine.toUpperCase()
+    }
+    if (req.body.weight) {
+      workout.weight = parseInt(workout.weight)
+    }
+    if (req.body.reps) {
+      workout.reps = parseInt(workout.reps)
+    }
+    if (req.body.sets) {
+      workout.sets = parseInt(workout.sets)
+    }
+
+    const newWorkout = await workout.save()
+    console.log(`newWorkout ==> ${newWorkout}`)
+    res.send(newWorkout)
+  } catch (error) {
+    console.error('Error creating workout', error)
+  }
 }
 
-async function deleteWorkout(req, res) {
-	const workout = await Workout.deleteOne({
-		workout_id: req.params.id,
-	});
-	res.send({
-		msg: 'Post Deleted',
-		payload: req.params.workout_id,
-		status: 'Ok',
-	});
+async function deleteOneWorkout(req, res) {
+  const workout = await Workout.deleteOne({
+    workout_id: req.params.id
+  })
+  res.send({
+    msg: 'Post Deleted',
+    payload: req.params.workout_id,
+    status: 'Ok'
+  })
 }
 
-async function updateWorkout(req, res) {
-	try {
-		console.log(req.body);
+async function updateOneWorkout(req, res) {
+  try {
+    console.log(req.body)
 
-		const updateWorkout = await Workout.findByIdAndUpdate(
-			req.params.workout_id,
-			{
-				title: req.body.title,
-				machine: req.body.machine,
-				reps: req.body.reps,
-				sets: req.body.sets,
-				weight: req.body.weight,
-			}
-		);
-		res.send(updateWorkout);
-	} catch (err) {
-		console.log(err);
-	}
+    const updateWorkout = await Workout.findByIdAndUpdate(
+      req.params.workout_id,
+      {
+        title: req.body.title,
+        machine: req.body.machine,
+        reps: req.body.reps,
+        sets: req.body.sets,
+        weight: req.body.weight
+      }
+    )
+    res.send(updateWorkout)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-async function createReview(req, res) {
-	try {
-		const workout = await Workout.findById(req.params.workout_id).populate(
-			'reviews'
-		);
-		// req.body.trainee = req.user._id
-		// req.body.userName = req.user.name
+async function createOneReview(req, res) {
+  try {
+    const workout = await Workout.findById(req.params.workout_id).populate(
+      'reviews'
+    )
 
-		console.log(`workout ${workout}`);
+    console.log(`workout ${workout}`)
 
-		const newReview = await Review.create(req.body);
-		workout.reviews.push(newReview._id);
-		await workout.save();
-		res.send(newReview);
-	} catch (err) {
-		console.log(err);
-	}
+    const newReview = await Review.create(req.body)
+    workout.reviews.push(newReview._id)
+    await workout.save()
+    res.send(newReview)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 const getAllReviews = async (req, res) => {
-	try {
-		const reviews = await Review.find({});
-		res.send(reviews);
-	} catch (error) {
-		throw error;
-	}
-};
-
-async function updateReview(req, res) {
-	try {
-		console.log(req.body);
-
-		const updatedReview = await Review.findByIdAndUpdate(req.params.review_id, {
-			reviewContent: req.body.reviewContent,
-			reviewRating: req.body.reviewRating,
-		});
-		res.send(updatedReview);
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    const workoutId = req.params.workoutId
+    if (!workoutId) {
+      return res.status(400).send('Missing workout ID in request')
+    }
+    const workout = await Workout.findById(workoutId).populate('reviews')
+    res.send(workout.reviews)
+  } catch (error) {
+    throw error
+  }
 }
 
-async function deleteReview(req, res) {
-	const review = await Review.deleteOne({
-		review_id: req.params.id,
-	});
-	res.send({
-		msg: 'Review Deleted',
-		payload: req.params.review_id,
-		status: 'Ok',
-	});
+async function updateOneReview(req, res) {
+  try {
+    console.log(req.body)
+
+    const updatedReview = await Review.findByIdAndUpdate(req.params.review_id, {
+      reviewContent: req.body.reviewContent,
+      reviewRating: req.body.reviewRating
+    })
+    res.send(updatedReview)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+async function deleteOneReview(req, res) {
+  const review = await Review.deleteOne({
+    review_id: req.params.id
+  })
+  res.send({
+    msg: 'Review Deleted',
+    payload: req.params.review_id,
+    status: 'Ok'
+  })
 }
 
 module.exports = {
-	getAllWorkouts,
-	create,
-	delete: deleteWorkout,
-	createReview,
-	getAllReviews,
-	updateReview,
-	deleteReview,
-	updateWorkout,
-};
+  getAllWorkouts,
+  createOneWorkout,
+  deleteOneWorkout,
+  createOneReview,
+  getAllReviews,
+  updateOneReview,
+  deleteOneReview,
+  updateOneWorkout
+}
--- routes/WorkoutsRouter.js
+++ routes/WorkoutsRouter.js
@@ -2,44 +2,44 @@ const router = require('express').Router()
 const middleware = require('../middleware')
 const workoutsCtrl = require('../controllers/workoutsCtrl')
 
-router.get('/', workoutsCtrl.getWorkout)
+router.get('/', workoutsCtrl.getAllWorkouts)
+
 router.post(
   '/',
   middleware.stripToken,
   middleware.verifyToken,
-  workoutsCtrl.create
+  workoutsCtrl.createOneWorkout
 )
 router.put(
   '/:workout_id',
   middleware.stripToken,
   middleware.verifyToken,
-  workoutsCtrl.updateWorkout
+  workoutsCtrl.updateOneWorkout
 )
 router.delete(
   '/:workout_id',
   middleware.stripToken,
   middleware.verifyToken,
-  workoutsCtrl.delete
+  workoutsCtrl.deleteOneWorkout
 )
 
+router.get('/:workoutId/reviews', workoutsCtrl.getAllReviews)
 router.post(
   '/:workout_id/reviews',
   middleware.stripToken,
   middleware.verifyToken,
-  workoutsCtrl.createReview
+  workoutsCtrl.createOneReview
 )
-router.get('/reviews/:review_id', workoutsCtrl.getReview)
 router.put(
   '/reviews/:review_id',
   middleware.stripToken,
   middleware.verifyToken,
-  workoutsCtrl.updateReview
+  workoutsCtrl.updateOneReview
 )
 router.delete(
   '/reviews/:review_id',
   middleware.stripToken,
   middleware.verifyToken,
-  workoutsCtrl.deleteReview
+  workoutsCtrl.deleteOneReview
 )
-
 module.exports = router
